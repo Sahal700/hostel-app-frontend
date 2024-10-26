ff16b3f9648bf67368ead3bcefdc1cd6dda7d439
Deepak050603 <pararampathdeepak@gmail.com>
2024-10-26 23:53:55 +0530

    roomc is started

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Roomc from '../components/Roomc'
 
 function Room() {
   return (
-    <div>
-      room
+    <div className='grid grid-cols-[repeat(1,3fr_1fr)] py-10'>
+    <div className='grid grid-cols-[repeat(3,1fr)]'>
+      <div><Roomc/></div>
+      <div><Roomc/></div>
+      <div><Roomc/></div>
     </div>
+
+    <div>
+      
+
+    </div>
+    
+  </div>
   )
 }
 
