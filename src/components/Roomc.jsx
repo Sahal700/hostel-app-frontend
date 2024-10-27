1fe55e01441ef676ab9c841315b5988d63ab5b8d
Sahal700 <mohammedsahal400@gmail.com>
2024-10-27 12:23:03 +0530

    made changes in roomc

--- src/components/Roomc.jsx
+++ src/components/Roomc.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 
 function Roomc() {
-    
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+  const f=false
   return (
     <>
-    <div className='border border-black p-5'>
+    <div className='border border-black p-3 mt-3'>
      
       <div className='flex items-center'>
-        <img src="https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png" className='rounded w-[50px]' alt="" />
-        <span>user 1</span>
-        payment 
-
+        <div className='flex items-center'>
+          <img src="https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png" className='rounded-[50%] w-[50px] me-2' alt="" />
+          <span>user 1</span>
+        </div>
+        <div className='ms-auto'>
+        <FormControlLabel control={<Checkbox {...label} defaultChecked={f} onChange={(e)=>{console.log(e.target.checked)}} color="success" />} label="payment" />
+        </div>
       </div>
       <div></div>
       <div></div>
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -3,11 +3,21 @@ import Roomc from '../components/Roomc'
 
 function Room() {
   return (
-    <div className='grid grid-cols-[repeat(1,3fr_1fr)] py-10'>
-    <div className='grid grid-cols-[repeat(3,1fr)]'>
-      <div><Roomc/></div>
-      <div><Roomc/></div>
-      <div><Roomc/></div>
+    <div className='grid grid-cols-[repeat(1,3fr_1fr)] py-10 px-3'>
+    <div className='grid grid-cols-[repeat(3,1fr)] gap-3'>
+      <div>
+        <h4 className='text-center text-xl' >room no: <span className='font-semibold'>1</span></h4>
+        <Roomc />
+        <Roomc />
+      </div>
+      <div>
+        <h4 className='text-center text-xl' >room no: <span className='font-semibold'>1</span></h4>
+        <Roomc/>
+      </div>
+      <div>
+        <h4 className='text-center text-xl' >room no: <span className='font-semibold'>1</span></h4>
+        <Roomc/>
+      </div>
     </div>
 
     <div>
