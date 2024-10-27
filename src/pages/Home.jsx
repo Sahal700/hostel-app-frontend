33415fa6ef226aa7ff2ca2232a8a4d1bbc85f83d
Deepak050603 <pararampathdeepak@gmail.com>
2024-10-28 00:07:09 +0530

    getallstudentapi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      <h1 className='text-center text-red-700'>home</h1>
+    <div className="">
+      <div className="row w-100 p-16">
+        <div className="col-4"></div>
+        <div className="col-4">
+          <div className='border flex items-center justify-between border-slate-400 p-3 rounded shadow '>
+            <img src="https://cdn3.iconfinder.com/data/icons/travel-emoji/50/HotelRoom-1024.png" className='w-[100px]' alt="" />
+            <div className='pe-4'>
+              <h4>Rooms</h4>
+              <p>Go  <FontAwesomeIcon icon={faArrowRight} /></p>
+            </div>
+          </div>
+          <div className=' border border-slate-400 p-5 mt-5 rounded shadow'>3</div>
+        </div>
+        <div className="col-4"></div>
+      </div>
     </div>
   )
 }
